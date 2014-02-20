ac9280624f7b38dcd15b6030b8cd131c57136b80
akella <akella.a@gmail.com>
2014-02-20 12:30:43 +0200

    upd

--- js/common.js
+++ js/common.js
@@ -1,37 +1,46 @@
 $(document).ready(function() {
+
+	$("select").selectBox();
+
+	$(document).click(function() {
+		$(".js-drop ul").hide();
+		$(".js-select-list").hide();
+		$(".js-select").removeClass("is-active");
+	}); 
+
 	function select() {
 		$(".js-select").each(function(){
 			var select_list = $(this).parent().find(".js-select-list");
 			var text = select_list.find("li").first().text();
 			$(this).find(".js-select-text").text(text);
 			$(this).click(function(event){
-					if ($(this).hasClass("is-active")) {
-						$(this).removeClass("is-active");
-						select_list.slideUp("fast");
-					}
-					else {
-						$(".js-select").removeClass("is-active");
-						$(".js-select-list").hide();
-						select_list.slideDown("fast");
-						$(this).addClass("is-active");
-					}
-					event.stopPropagation();
-				});
-				select_list.find("li").click(function(event) {
-					var id = $(this).attr("data-id");
-					var text = $(this).text();
-					$(this).parent().parent().find(".js-select-text").text(text);
-					$(this).parent().parent().find(".js-select-input").val(id);
-					$(this).parent().hide();
-					$(this).parents(".js-select").removeClass("is-active");
-					event.stopPropagation();
-				});
+				if ($(this).hasClass("is-active")) {
+					$(this).removeClass("is-active");
+					select_list.slideUp("fast");
+				}
+				else {
+					$(".js-select").removeClass("is-active");
+					$(".js-select-list").hide();
+					select_list.slideDown("fast");
+					$(this).addClass("is-active");
+				}
+				event.stopPropagation();
 			});
-		}
-		select();
-		$('.js-select').click(function(event){
-			event.stopPropagation();
+			select_list.find("li").click(function(event) {
+				var id = $(this).attr("data-id");
+				var text = $(this).text();
+				$(this).parent().parent().find(".js-select-text").text(text);
+				$(this).parent().parent().find(".js-select-input").val(id);
+				$(this).parent().hide();
+				$(this).parents(".js-select").removeClass("is-active");
+				event.stopPropagation();
+			});
 		});
+	}
+	select();
+	$('.js-select').click(function(event){
+		event.stopPropagation();
+	}); 
 
 
 						   
@@ -47,7 +56,7 @@ $(document).ready(function() {
 	$(".payment__form").validate({
 		rules: {
 			card_number: {
-				rangelength: [1, 21]
+				rangelength: [19, 21]
 			}
 		},
 		messages:{
